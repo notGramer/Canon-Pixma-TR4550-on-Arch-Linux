241a99b6f27642bf0a490ee546097bc6f1a58fa2
notGramer <kiourtzidis.jiannis@gmail.com>
2025-11-08 22:50:40 +0100

    update Install Script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+sudo pacman -Syu
 sudo pacman -S cups cups-pdf ghostscript gsfonts system-config-printer
 sudo systemctl enable --now cups.service
 sudo systemctl enable --now cups.socket cups.path
