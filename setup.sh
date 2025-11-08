2b2afe7303694b61c846efda364885faadb9e13b
notGramer <kiourtzidis.jiannis@gmail.com>
2025-11-08 22:51:15 +0100

    delete update Install Script

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-sudo pacman -Syu
-sudo pacman -S cups cups-pdf ghostscript gsfonts system-config-printer
-sudo systemctl enable --now cups.service
-sudo systemctl enable --now cups.socket cups.path
-sudo systemctl status cups.service
